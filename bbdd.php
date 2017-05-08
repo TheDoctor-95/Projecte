6c220833998ec30b868ffaefbe8aa41871b7714a
usu67 <usu67@A36O15.novaulas.stucom.com>
2017-05-08 13:40:56 +0200

    Merge origin/master
    Conflicts:
    	bbdd2.php

--- bbdd.php
+++ bbdd.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function conectar($database) {
     $con = mysqli_connect("localhost", "root", "", "webmusica")
             or die("No se ha podido conctar con la BBDD.");
@@ -8,54 +7,49 @@ function conectar($database) {
     return $con;
 }
 
-function desconectar ($conexion){
+function desconectar($conexion) {
     mysqli_close($conexion);
-    
 }
 
 function ProximsConcerts() {
     //Conectamos con la bbdd
     $con = conectar("webmusica");
     //Definimos la consulta
-    $query ="select nom,data_concert from concerts order by data_concert desc";
+    $query = "select nom,data_concert from concerts order by data_concert desc";
     //Ejecutamos la consulta, recogiendo el resultado
-    $resultado =mysqli_query($con,$query);
+    $resultado = mysqli_query($con, $query);
     //desconectamos de la bbdd
-    desconectar ($con);
+    desconectar($con);
     //devolvemos el resultado de la consulta
     return $resultado;
-    
 }
 
 function VotarConcert() {
     //Conectamos con la bbdd
     $con = conectar("webmusica");
     //Definimos la consulta
-    $query ="select id_concert,nom, data_concert from concerts ;";
-    
+    $query = "select nom_usuari,nom, data_concert, id_concert from concerts ;";
+
     //Ejecutamos la consulta, recogiendo el resultado
-    $resultado =mysqli_query($con,$query);
+    $resultado = mysqli_query($con, $query);
     //desconectamos de la bbdd
-    desconectar ($con);
+    desconectar($con);
     //devolvemos el resultado de la consulta
     return $resultado;
-    
 }
 
 function VotarMusic() {
     //Conectamos con la bbdd
     $con = conectar("webmusica");
     //Definimos la consulta
-    $query ="select nom_usuari from usuaris where classe_usuari='Music'";
+    $query = "select nom_usuari from usuaris where classe_usuari='Music'";
     //Ejecutamos la consulta, recogiendo el resultado
-    $resultado =mysqli_query($con,$query);
+    $resultado = mysqli_query($con, $query);
     //desconectamos de la bbdd
-    desconectar ($con);
+    desconectar($con);
     //devolvemos el resultado de la consulta
     return $resultado;
-    
 }
-
 
 function insertarVotacioMusic($nom_usuari, $votacio) {
 
@@ -71,11 +65,11 @@ function insertarVotacioMusic($nom_usuari, $votacio) {
     desconectar($con);
 }
 
-function insertarVotacioConcert($votaciomusic) {
-
+function insertarVotacioConcert($id_concert, $votacio) {
 
     $con = conectar("webmusica");
-    $insert = "insert into votarconcert values ('fan1', 'music1', '$votaciomusic')";
+    $insert = "update user set votacio=(votacio+$votacio) where id_concert='$id_concert'";
+
     if (mysqli_query($con, $insert)) {
         echo "<p>Votació correcte</p>";
     } else {
